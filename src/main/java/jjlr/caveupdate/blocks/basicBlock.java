a7d9f1ddcb5fcde03a2bb2f9658e7e1ae4b65874
jjlr <mc.jjlr@tutanota.com>
2018-06-24 19:37:51 -0700

    Added basicBlock.java with no content

---
+++ src/main/java/jjlr/caveupdate/blocks/basicBlock.java
@@ -0,0 +1,13 @@
+package jjlr.caveupdate.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class basicBlock extends Block {
+
+	public basicBlock(String name, Material material) {
+		super(material);
+		
+	}
+
+}
