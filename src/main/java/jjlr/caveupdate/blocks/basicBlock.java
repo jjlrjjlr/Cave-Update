ec9fb931ead271aa7d7cf61e3c3d820c7960645b
jjlr <mc.jjlr@tutanota.com>
2018-06-24 20:25:15 -0700

    Completed basicBlock.java

--- src/main/java/jjlr/caveupdate/blocks/basicBlock.java
+++ src/main/java/jjlr/caveupdate/blocks/basicBlock.java
@@ -1,12 +1,43 @@
 package jjlr.caveupdate.blocks;
 
+import jjlr.caveupdate.caveUpdateMain;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 
 public class basicBlock extends Block {
+	
+	protected String name;
 
 	public basicBlock(String name, Material material) {
+		
 		super(material);
+		
+		this.name = name;
+		
+		setUnlocalizedName(name);
+		setRegistryName(name);
+	}
+	
+	public void registerItemModel(Item itemBlock) {
+		
+		caveUpdateMain.proxy.registerItemRenderer(itemBlock, 0, name);
+		
+	}
+	
+	public Item createItemBlock() {
+		
+		return new ItemBlock(this).setRegistryName(getRegistryName());
+		
+	}
+	
+	@Override
+	public basicBlock setCreativeTab(CreativeTabs tab) {
+		
+		super.setCreativeTab(tab);
+		return this;
 		
 	}
 
